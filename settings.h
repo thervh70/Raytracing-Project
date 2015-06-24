38081eb1370db240862f07a2006ed30f9037a084
thervh70 <R.H.vanHeukelum@student.tudelft.nl>
2015-06-25 00:48:07 +0200

    Changed settings

--- settings.h
+++ settings.h
@@ -5,15 +5,15 @@
 #define SETTINGS_H_
 
 const float backgroundlighting = 0.34f;
-const float diffusePower = 1.2f;
+const float diffusePower = 0.5f;
 //const float specularHardness = 67.f; // this is stored in the .mtl file
-const float ShadowFactor = 0.6f;
+const float ShadowFactor = 0.5f;
 
 const float shadowRadius = 0.1f; // This is the "radius" of the light source
 const int shadowSamples = 4; // This results in shadowSamples ^ 3 shadowrays being shot, set to 1 for hard shadows
 
-const int MSAA = 1; //valid: 1, 4, 16; default: 1
-const float depthPower = 12.0f; // default: 4.0f
+const int MSAA = 16; //valid: 1, 4, 16; default: 1
+const float depthPower = 2.5f; // default: 4.0f
 
 #define backgroundColor Vec3Df(0.2f, 0.5f, 1.0f)
 #define endOfReflection Vec3Df(0.0f, 0.0f, 0.0f)
